56ed2ee7e0e5892267ee7f43aac1fbfbdca9ffd5
Eric Black <bobcrotch@gmail.com>
2011-06-12 12:49:47 -0700

    working connect string with ssl

--- lib/connect.rb
+++ lib/connect.rb
@@ -1,6 +1,25 @@
 #!/home/eblack/.rvm/rubies/ruby-1.9.2-p180/bin/ruby
+
 require 'savon/client'
+
+endpoints = {
+    :wsdl => "https://host/sdk/vim.wsdl",
+    :endpoint => "https://host/sdk/vimService"
+}
 client = Savon::Client.new do
-  wsdl.document = "https://phxvc.domestic.guitarcenter.com/sdk/vim.wsdl"
+  wsdl.document = "https://host/sdk/vim.wsdl"
+  wsdl.endpoint = "https://host/sdk/vimService"
+  http.auth.ssl.cert_file = 'host.cert'
+  http.auth.ssl.verify_mode = :none
 end
-client.wsdl.soap_actions
+#  WSSE auth doesn't appear to work..
+#  But if it did the following would be appropriate
+#  client.wsse.credentials "username", "strongpass"
+request = client.request(:urn, 'QueryDateTime' ) { 
+  soap.body = {
+    "_this type='dateTime'" => "dateTime"
+}
+}
+
+puts request.to_xml
+
